94c5d7caca44698808b5856165ed9ca425fb335d
ashu <ashwinsariwal007@gmail.com>
2020-05-02 19:21:01 +0530

    host4

--- home/models.py
+++ home/models.py
@@ -7,7 +7,7 @@ class Contact(models.Model):
     sno = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
-    phone = models.IntegerField()
+    phone = models.CharField(max_length=50, default="")
     query = models.TextField()
     timeStamp = models.DateTimeField(auto_now_add=True, blank=True)
 
